0142a47288b1339f2ddf323ab74c2994d1478b27
Fisihatsion <fisihatsion19@gmail.com>
2022-06-02 01:34:17 -0700

    M

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.io>
+
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * -Dora Korpar, 2015-10-19,
+ *  followed by a new line, to the standard error.
+ *  Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	wirte(2, "and that piece of art is usefule\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
